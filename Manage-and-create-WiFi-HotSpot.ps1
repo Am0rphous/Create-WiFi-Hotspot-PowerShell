617de24c4a5228e0637e4d232dcabcca2e32f6f2
Henrik Johnsen <29645801+Am0rphous@users.noreply.github.com>
2017-07-31 13:28:47 +0200

    Update Manage-and-create-WiFi-HotSpot.ps1

--- Manage-and-create-WiFi-HotSpot.ps1
+++ Manage-and-create-WiFi-HotSpot.ps1
@@ -47,22 +47,24 @@ Function LoadMenu {
     while ( $menuOption -lt 1 -or $menuOption -gt $LastOption ) {
         CLS #Clears the creen
         Write-Host $MenuBar                            -Fore Magenta
-        Write-Host "`n`tChoose between these options:" -Fore Cyan
-        Write-host "$nt`1. " -NoNewline -Fore Cyan; Write-Host "Show available drivers"
-        Write-host "$nt`2. " -NoNewline -Fore Cyan; Write-Host "View hosted network settings"
-        Write-host "$nt`3. " -NoNewline -Fore Cyan; Write-Host "Configure Wifi HotSpot"
-        Write-host "$nt`4. " -NoNewline -Fore Cyan; Write-Host "Show Wireless LAN settings"
-        Write-host "$nt`5. " -NoNewline -Fore Cyan; Write-Host "Display blocked networks"
-        Write-host "$nt`6. " -NoNewline -Fore Cyan; Write-Host "Show info about interfaces"
-        Write-host "$nt`7. " -NoNewline -Fore Cyan; Write-Host "Display all information"
+        Write-Host "`n`tgithub.com/Am0rphous" 
+        Write-Host "$nt`Choose between these options:" -Fore Cyan
+        Write-host "$nt`1. " -NoNewline -Fore Cyan; Write-Host "Configure a Wifi HotSpot"
+        Write-host "$nt`2. " -NoNewline -Fore Cyan; Write-Host "Start Wifi HotSpot"
+        Write-host "$nt`3. " -NoNewline -Fore Cyan; Write-Host "Stop Wifi HotSpot"
+        Write-host "$nt`4. " -NoNewline -Fore Cyan; Write-Host "View hosted network settings"
+        Write-host "$nt`5. " -NoNewline -Fore Cyan; Write-Host "Show Wireless LAN settings"
+        Write-host "$nt`6. " -NoNewline -Fore Cyan; Write-Host "Display blocked networks"
+        Write-host "$nt`7. " -NoNewline -Fore Cyan; Write-Host "Show info about interfaces"
+        Write-host "$nt`8. " -NoNewline -Fore Cyan; Write-Host "Display all information"
         Write-host "$t   - Displays the entire collection of information about wireless 
              network adapters, wireless profiles and wireless networks"
-        Write-host "$nt`8. " -NoNewline -Fore Cyan; Write-Host "Start Wifi HotSpot"
-        Write-host "$nt`9. " -NoNewline -Fore Cyan; Write-Host "Stop Wifi HotSpot"
-        Write-host "$nt`10. " -NoNewline -Fore Cyan; Write-Host "Exit`n"
+        Write-host "$nt`9. " -NoNewline -Fore Cyan; Write-Host "Show available drivers"
+        
+        Write-host "$nt`10. " -NoNewline -Fore Cyan; Write-Host "Exit"
 
     #Gets input which is supposed to an integer value from the user
-    [Int] $menuOption = Read-Host "`tOption"
+    [Int] $menuOption = Read-Host "`n`tOption"
         if ( $menuOption -lt 1 -or $menuOption -gt $LastOption ) {
 			Write-Host "$nt`Please choose a number in the menu" -Fore Red
             Sleep -Seconds 2 #Script pauses for two seconds, so the user has time to read the error message
@@ -73,32 +75,7 @@ Function LoadMenu {
 
     Switch ( $menuOption ) {
 
-    1 { #Option 1 - Show available drivers
-        Try {
-            $Drivers = netsh wlan show drivers
-                if ($Drivers) {
-                    Write-Host "`tFound folowing drivers" -Fore Green
-                    $Drivers
-                    Write-Host "`tMake sure to check if the drivers support hosting a network`n" -Fore Yellow
-                }
-                else { Write-Host "`tCouldn't find any drivers" -Fore Red }
-        } Catch { CatchErrorMessage }
-
-        #Prompts the user to push 'enter' to go back to main menu
-        Prompt_reload_menu
-
-    } #Option 1 - Show available drivers
-
-    2 { #Option 2 - View hosted network settings
-
-        Try { netsh wlan show hostednetwork }
-        Catch { CatchErrorMessage }
-
-        Prompt_reload_menu
-
-    } #Option 2 - View hosted network settings
-
-    3 { #Option 3 - Configure Wifi HotSpot
+    1 { #Option 1 - Configure Wifi HotSpot
 
         #First - Specify a network name
         Do {
@@ -141,13 +118,11 @@ Function LoadMenu {
 
         } while ($Answer -ne "y" -and $Answer -ne "n")
 
-
-
         #Checks if the user answered 'n' for 'no' to starting the network right away
         If ($Answer -eq "n") {
             Reload_menu_now
         }
-        else { #if user didn't answer 'n', he/she wishes then to start the network
+        else { #if user didn't answer 'n' then he/she wishes then to start the network
             Try {
                 Write-Host ""
                 netsh wlan start hostednetwork
@@ -155,49 +130,9 @@ Function LoadMenu {
             } Catch { CatchErrorMessage }
         }
 
-    } #Option 3 - Configure Wifi HotSpot
+    } #Option 1 - Configure Wifi HotSpot
 
-    4 { #Option 4 - Show Wireless LAN settings
-
-        Try {
-            netsh wlan show settings #Displays settings
-            Prompt_reload_menu
-        }
-
-        Catch { CatchErrorMessage }
-    } #Option 4 - Show Wireless LAN settings
-
-    5 { #Option 5 - display blocked networks
-
-        Try { netsh wlan show blockednetworks }
-
-        Catch { CatchErrorMessage }
-
-        Prompt_reload_menu
-
-    } #Option 5 - display blocked networks
-
-    6 { #Option 6 - Show info about interfaces
-
-        Try { netsh wlan show interfaces }
-
-        Catch { CatchErrorMessage }
-
-        Prompt_reload_menu
-
-    } #Option 6 - Show info about interfaces
-
-    7 { #Option 7 - Show info about interfaces
-
-        Try { netsh wlan show all }
-
-        Catch { CatchErrorMessage }
-
-        Prompt_reload_menu
-
-    } #Option 7 - Show info about interfaces
-
-    8 { #Option 8 - Start Wifi HotSpot
+    2 { #Option 2 - Start Wifi HotSpot
 
         Try { netsh wlan start hostednetwork }
 
@@ -205,9 +140,9 @@ Function LoadMenu {
 
         Prompt_reload_menu
 
-    } #Option 8 - Start Wifi HotSpot
+    } #Option 2 - Start Wifi HotSpot
 
-    9 { #Option 9 - Stop Wifi HotSpot
+    3 { #Option 3 - Stop Wifi HotSpot
 
         Try { netsh wlan stop hostednetwork }
 
@@ -215,7 +150,74 @@ Function LoadMenu {
 
         Prompt_reload_menu
 
-    } #Option 9 - Stop Wifi HotSpot
+    } #Option 3 - Stop Wifi HotSpot
+
+    4 { #Option 4 - View hosted network settings
+
+        Try { netsh wlan show hostednetwork }
+        Catch { CatchErrorMessage }
+
+        Prompt_reload_menu
+
+    } #Option 4 - View hosted network settings
+
+    5 { #Option 5 - Show Wireless LAN settings
+
+        Try {
+            netsh wlan show settings #Displays settings
+            Prompt_reload_menu
+        }
+
+        Catch { CatchErrorMessage }
+
+    } #Option 5 - Show Wireless LAN settings
+
+    6 { #Option 6 - display blocked networks
+
+        Try { netsh wlan show blockednetworks }
+
+        Catch { CatchErrorMessage }
+
+        Prompt_reload_menu
+
+    } #Option 6 - display blocked networks
+
+    7 { #Option 7 - Show info about interfaces
+
+        Try { netsh wlan show interfaces }
+
+        Catch { CatchErrorMessage }
+
+        Prompt_reload_menu
+
+    } #Option 7 - Show info about interfaces
+
+    8 { #Option 8 - Show info about interfaces
+
+        Try { netsh wlan show all }
+
+        Catch { CatchErrorMessage }
+
+        Prompt_reload_menu
+
+    } #Option 8 - Show info about interfaces
+
+    9 { #Option 9 - Show available drivers
+
+        Try {
+            $Drivers = netsh wlan show drivers
+                if ($Drivers) {
+                    Write-Host "`tFound folowing drivers" -Fore Green
+                    $Drivers
+                    Write-Host "`tMake sure to check if the drivers support hosting a network`n" -Fore Yellow
+                }
+                else { Write-Host "`tCouldn't find any drivers" -Fore Red }
+        } Catch { CatchErrorMessage }
+
+        #Prompts the user to push 'enter' to go back to main menu
+        Prompt_reload_menu
+
+    } #Option 9 - Show available drivers
     
     default { #Code to execute if option number 10 is chosen 
         Write-Host "`t __________________ "
