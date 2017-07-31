b3261aec5d76d61e88b040cc8c56d17fa88dbbe1
Henrik Johnsen <29645801+Am0rphous@users.noreply.github.com>
2017-07-31 03:06:11 +0200

    Add files via upload

---
+++ Manage-WiFi-Hotspot.ps1
@@ -0,0 +1,10 @@
+# netsh interface set interface "Wi-Fi 2" newname="Alfa"
+
+
+netsh wlan set hostednetwork mode=allow ssid=sloppySeconds key=Heilurifax1?til
+netsh wlan start hostednetwork
+# netsh wlan stop hostednetwork
+
+# netsh interface set interface "Local Area Connection" newname="Management"
+# netsh interface ipv4 set address "Management" static 192.168.10.101 255.255.255.0 192.168.10.1
+# netsh -h #for hjelp
